6ef7df3729cf7abe4ef8e2ca99db808821cfb5af
viniciusscampos <silvcamposvinicius@gmail.com>
2016-12-13 00:04:57 -0200

    :new: Gradient method implemented and tested.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def fone():
 
 def ftwo():
     def g(p):
-        return arctan(-np.log(p[0]*(1-p[0])*p[1]*(1-p[1])))
+        return np.arctan(-np.log(p[0]*(1-p[0])*p[1]*(1-p[1])))
     return g
 
 def fslide():
@@ -36,12 +36,25 @@ def armijo(f,gf,x,d,n,y):
 		t= y*t
 	return t
 
-f = fslide()
-gf = gfslide()
-x = np.array([1,0])
-d = np.array([3,1])
+def metodo_gradiente(f,gf,x0,n,y):
+    k = 0
+    xk = x0
+    # array.all() retorna false e ao menos um valor do array for false
+    while(not np.isclose([gf(xk)],[1e-10,1e-11]).all()):
+        dk = -np.transpose(gf(xk))
+        tk = armijo(f,gf,xk,dk,n,y)
+        xprox = xk + tk*dk
+        k = k+1
+        xk = xprox        
+    return (k,xk,f(xk),gf(xk))    
+
+fone = fone()
+ftwo = ftwo()
+gfone= gfone()
+gftwo= gftwo()
+x = np.array([0.45,0.51])
 n = 0.25
-t = 1
 y = 0.8
 
-print(x +armijo(f,gf,x,d,n,y)*d)
+print(metodo_gradiente(fone,gfone,x,n,y))
+print(metodo_gradiente(ftwo,gftwo,x,n,y))
